6b85437b72d037d2e80cb0368d1c424aa48b51b3
MonkeyJerr <ajm3@andrew.cmu.edu>
2022-07-27 04:54:01 -0400

    Top Artists tab part 1

--- src/Top.js
+++ src/Top.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Top.css";
+import artistTab from './images/ArtistTab.png';
+
 
 const Top = props => {
   const tracks_color = props.current_color[2];
@@ -21,19 +23,33 @@ const Top = props => {
           </div>
         </div>
 
-        <div className="artists-box" style={{backgroundColor: artists_color}}>
+        <div className="artists-box justify-center" style={{backgroundImage: `url(${artistTab})` }}>
           <div className="top-artists">
-            <h2 className="top-artists-header">top artists</h2>
             <div className="top-artist-img">
               <img className="items-center" src={props.top_artist_items[0].images[0].url} />
             </div>
-            <ol className="top-artists-list">
-                <li>{props.top_artist_items[0].name}</li>
-                <li>{props.top_artist_items[1].name}</li>
-                <li>{props.top_artist_items[2].name}</li>
-                <li>{props.top_artist_items[3].name}</li>
-                <li>{props.top_artist_items[4].name}</li>
-            </ol>
+            <div className="top-artists-list grid grid-flow-col">
+              <div className = "flex flex-col">
+                <img className="items-center" src={props.top_artist_items[0].images[0].url} />
+                <div>{props.top_artist_items[0].name}</div>
+              </div>
+              <div className = "flex flex-col">
+                <img className="items-center" src={props.top_artist_items[1].images[1].url} />
+                <div>{props.top_artist_items[1].name}</div>
+              </div>
+              <div className = "flex flex-col">
+                <img className="items-center" src={props.top_artist_items[2].images[2].url} />
+                <div>{props.top_artist_items[2].name}</div>
+              </div>
+              <div className = "flex flex-col">
+                <img className="items-center" src={props.top_artist_items[2].images[2].url} />
+                <div>{props.top_artist_items[2].name}</div>
+
+              </div>
+
+
+
+            </div>
           </div>
         </div>
       </div>
