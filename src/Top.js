d4e02014310e49c7dedd42298d52edd1c9bbf7e9
MonkeyJerr <ajm3@andrew.cmu.edu>
2022-07-27 23:02:29 -0400

    Artist tab part 2

--- src/Top.js
+++ src/Top.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import "./Top.css";
 import artistTab from './images/ArtistTab.png';
 
@@ -6,6 +7,7 @@ import artistTab from './images/ArtistTab.png';
 const Top = props => {
   const tracks_color = props.current_color[2];
   const artists_color = props.current_color[3];
+  const [selectedImg , setSelectedImg] = useState(props.top_artist_items[0].images[0].url);
 
   return (
     <div className="App">
@@ -26,25 +28,38 @@ const Top = props => {
         <div className="artists-box justify-center" style={{backgroundImage: `url(${artistTab})` }}>
           <div className="top-artists">
             <div className="top-artist-img">
-              <img className="items-center" src={props.top_artist_items[0].images[0].url} />
+              <img className="items-center" src={selectedImg} />
             </div>
             <div className="top-artists-list grid grid-flow-col">
               <div className = "flex flex-col">
-                <img className="items-center" src={props.top_artist_items[0].images[0].url} />
+                <img
+                  style= {{border: selectedImg === props.top_artist_items[0].images[0].url ? "4px solid purple" : ""}} 
+                  className="items-center" 
+                  src={props.top_artist_items[0].images[0].url} 
+                  alt= "ArtistOne"
+                  onClick = {() => setSelectedImg(props.top_artist_items[0].images[0].url)}
+                />
                 <div>{props.top_artist_items[0].name}</div>
               </div>
               <div className = "flex flex-col">
-                <img className="items-center" src={props.top_artist_items[1].images[1].url} />
+              <img
+                  style= {{border: selectedImg === props.top_artist_items[1].images[0].url ? "4px solid purple" : ""}} 
+                  className="items-center" 
+                  src={props.top_artist_items[1].images[0].url} 
+                  alt= "ArtistTwo"
+                  onClick = {() => setSelectedImg(props.top_artist_items[1].images[0].url)}
+                />
                 <div>{props.top_artist_items[1].name}</div>
               </div>
               <div className = "flex flex-col">
-                <img className="items-center" src={props.top_artist_items[2].images[2].url} />
+              <img
+                  style= {{border: selectedImg === props.top_artist_items[2].images[0].url ? "4px solid purple" : ""}} 
+                  className="items-center" 
+                  src={props.top_artist_items[2].images[0].url} 
+                  alt= "ArtistTwo"
+                  onClick = {() => setSelectedImg(props.top_artist_items[2].images[0].url)}
+                />
                 <div>{props.top_artist_items[2].name}</div>
-              </div>
-              <div className = "flex flex-col">
-                <img className="items-center" src={props.top_artist_items[2].images[2].url} />
-                <div>{props.top_artist_items[2].name}</div>
-
               </div>
 
 
